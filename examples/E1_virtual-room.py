7915760311aa78b3993a1abc639704c088b23325
GianMarcoDeBortoli <gian.debortoli@aalto.fi>
2026-03-03 16:54:37 +0200

    Minor change to an example

--- examples/E1_virtual-room.py
+++ examples/E1_virtual-room.py
@@ -3,7 +3,6 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import matplotlib.pyplot as plt
 # FLAMO
 from flamo import dsp
 from flamo.functional import signal_gallery
